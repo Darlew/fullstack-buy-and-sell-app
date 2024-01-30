d2eb7a3fe1bc7fee6c27ce49dfde416fe7ed0bdc
Darcy Lewis <darlew@hotmail.co.uk>
2024-01-31 07:14:42 +1100

    ACTUALLY connect Listing Detail page to backend

--- buy-and-sell/src/app/listing-detail-page/listing-detail-page.component.ts
+++ buy-and-sell/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, ActivatedRoute } from '@angular/router';
-import { fakeListings } from '../fake-data';
 import { Listing } from '../types';
+import { ListingsService } from '../listings.service';
+import { NgIf } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-listing-detail-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf, HttpClientModule],
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.css',
 })
 export class ListingDetailPageComponent implements OnInit {
+  isLoading: boolean = true;
   listing: Listing;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private listingsService: ListingsService
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.listingsService.getListingById(id).subscribe((listing) => {
+      this.listing = listing;
+      this.isLoading = false;
+    });
+    this.listingsService
+      .addViewToListing(id)
+      .subscribe(() => console.log('Views updated!'));
   }
 }
--- buy-and-sell/src/app/listings.service.ts
+++ buy-and-sell/src/app/listings.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Listing } from './types';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaderResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const HttpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +24,17 @@ export class ListingsService {
 
   getListings(): Observable<Listing[]> {
     return this.http.get<Listing[]>('/api/listings');
+  }
+
+  getListingById(id: string): Observable<Listing> {
+    return this.http.get<Listing>(`/api/listings/123`);
+  }
+
+  addViewToListing(id: string): Observable<Listing> {
+    return this.http.post<Listing>(
+      `/api/listings/${id}/add-view`,
+      {},
+      HttpOptions
+    );
   }
 }
