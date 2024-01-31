8191f6f745af93a47920bd7e051d604085ad22bd
Darcy Lewis <darlew@hotmail.co.uk>
2024-02-01 08:35:54 +1100

    Connected new listing page

--- buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
+++ buy-and-sell/src/app/new-listing-page/new-listing-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { ListingDataFormComponent } from '../listing-data-form/listing-data-form.component';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -11,10 +12,16 @@ import { ListingDataFormComponent } from '../listing-data-form/listing-data-form
   styleUrl: './new-listing-page.component.css',
 })
 export class NewListingPageComponent {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private listingsService: ListingsService
+  ) {}
 
-  onSubmit(): void {
-    alert('Creating a new listing...');
-    this.router.navigateByUrl('/my-listings');
+  onSubmit({ name, description, price }): void {
+    this.listingsService
+      .createListing(name, description, price)
+      .subscribe(() => {
+        this.router.navigateByUrl('/my-listings');
+      });
   }
 }
